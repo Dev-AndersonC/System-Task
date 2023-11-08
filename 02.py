f982f3b8dd6b544d8f776bf38ebb62df0136b042
Andersonc <andersoncsilva007@gmail.com>
2023-11-07 22:09:14 -0300

    test calc 02

---
+++ 02.py
@@ -0,0 +1,11 @@
+print('Hello world!')
+while True:
+    numb_02 = input('Digite seu multiplicador: ')
+    numb_03 = input('Até onde vai a tabuada: ')
+    
+    for c in range(int(numb_03)):
+        print(f'{c} x {numb_02} = ')
+    
+    resu = int(c) * int(numb_02)
+    
+    
